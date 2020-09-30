1ec2686e0fade09a998a041f0c608cf940e71d54
Igor Kichuk <66531978+kichman77@users.noreply.github.com>
2020-09-30 18:51:46 +0300

    timer without css

---
+++ index.js
@@ -0,0 +1,59 @@
+const timerRefs = {
+  daysSpan: document.querySelector('[data-value="days"]'),
+  hoursSpan: document.querySelector('[data-value="hours"]'),
+  minutesSpan: document.querySelector('[data-value="mins"]'),
+  secondsSpan: document.querySelector('[data-value="secs"]'),
+};
+
+class CountdownTimer {
+  constructor(date) {
+    this.selector = date.selector;
+    this.targetDate = date.targetDate;
+    this.deltaTime = 0;
+  }
+  updateClockFace(time) {
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+    timerRefs.daysSpan.textContent = days;
+    timerRefs.hoursSpan.textContent = hours;
+    timerRefs.minutesSpan.textContent = mins;
+    timerRefs.secondsSpan.textContent = secs;
+  }
+  start() {
+    setInterval(() => {
+      const currentDate = Date.now();
+      // console.log(currentDate);
+      this.deltaTime = this.targetDate - currentDate;
+      // console.log(this.deltaTime);
+      this.updateClockFace(this.deltaTime)
+    },1000)
+  }
+}
+
+new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Oct 15, 2020'),
+}).start()
+
+/*
+ * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
+ * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
+ */
+
+/*
+ * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
+ * остатка % и делим его на количество миллисекунд в одном часе
+ * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
+ */
+
+/*
+ * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
+ * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
+ */
+
+/*
+ * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
+ * миллисекунд в одной секунде (1000)
+ */
